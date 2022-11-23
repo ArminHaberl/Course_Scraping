b693fcf549f38e0774071fd4525f9c85243a3ff3
ArminHaberl <80152869+ArminHaberl@users.noreply.github.com>
2022-11-23 17:31:31 +0100

    change tryCatch to try()

--- One_Course_Scraping.R
+++ One_Course_Scraping.R
@@ -8,40 +8,51 @@ scrape_data <- function(course_id){
   course_json <- content(course_data)
   course_details <- course_json[["resource"]][[1]][["content"]][["cpCourseDetailDto"]]
   
-  tryCatch(id <- course_details[["cpCourseDto"]][["id"]],error=function(err){id <<- NA} ,warning=function(err){id <<- NA})
-  if (is.null(id)) { id <<- NA}
+  id <- NA
+  try(id <- course_details[["cpCourseDto"]][["id"]],silent = TRUE)
+  if (is.null(id)) { id <- NA}
   
-  tryCatch(course_number <- course_details[["cpCourseDto"]][["courseNumber"]][["databaseValue"]],error=function(err){course_number <<- NA} ,warning=function(err){course_number <<- NA})
-  if (is.null(course_number)) { course_number <<- NA}
+  course_number <- NA
+  try(course_number <- course_details[["cpCourseDto"]][["courseNumber"]][["databaseValue"]],silent = TRUE)
+  if (is.null(course_number)) { course_number <- NA}
   
-  tryCatch(semester <- course_details[["cpCourseDto"]][["semesterDto"]][["key"]],error=function(err){semester <<- NA} ,warning=function(err){semester <<- NA})
-  if (is.null(semester)) { semester <<- NA}
+  semester <- NA
+  try(semester <- course_details[["cpCourseDto"]][["semesterDto"]][["key"]],silent = TRUE)
+  if (is.null(semester)) { semester <- NA}
   
-  tryCatch(title_ge <- course_details[["cpCourseDto"]][["courseTitle"]][["translations"]][["translation"]][[1]][["value"]],error=function(err){title_ge <<- NA} ,warning=function(err){title_ge <<- NA})
-  if (is.null(title_ge)) { title_ge <<- NA}
+  title_ge <- NA
+  try(title_ge <- course_details[["cpCourseDto"]][["courseTitle"]][["translations"]][["translation"]][[1]][["value"]],silent = TRUE)
+  if (is.null(title_ge)) { title_ge <- NA}
   
-  tryCatch(title_en <- course_details[["cpCourseDto"]][["courseTitle"]][["translations"]][["translation"]][[2]][["value"]],error=function(err){title_en <<- NA} ,warning=function(err){title_en <<- NA})
-  if (is.null(title_en)) { title_en <<- NA}
+  title_en <- NA
+  try(title_en <- course_details[["cpCourseDto"]][["courseTitle"]][["translations"]][["translation"]][[2]][["value"]],silent = TRUE)
+  if (is.null(title_en)) { title_en <- NA}
   
-  tryCatch(ECTS <- course_details[["cpCourseDto"]][["ectsCredits"]],error=function(err){ECTS <<- NA} ,warning=function(err){ECTS <<- NA})
-  if (is.null(ECTS)) { ECTS <<- NA}
+  ECTS <- NA
+  try(ECTS <- course_details[["cpCourseDto"]][["ectsCredits"]],silent = TRUE)
+  if (is.null(ECTS)) { ECTS <- NA}
   
-  tryCatch(organisation_ge <- course_details[["cpCourseDto"]][["organisationResponsibleDto"]][["name"]][["translations"]][["translation"]][[1]][["value"]],error=function(err){organisation_ge <<- NA} ,warning=function(err){organisation_ge <<- NA})
-  if (is.null(organisation_ge)) { organisation_ge <<- NA}
+  organisation_ge <- NA
+  try(organisation_ge <- course_details[["cpCourseDto"]][["organisationResponsibleDto"]][["name"]][["translations"]][["translation"]][[1]][["value"]],silent = TRUE)
+  if (is.null(organisation_ge)) { organisation_ge <- NA}
   
-  tryCatch(organisation_en <- course_details[["cpCourseDto"]][["organisationResponsibleDto"]][["name"]][["translations"]][["translation"]][[2]][["value"]],error=function(err){organisation_en <<- NA} ,warning=function(err){organisation_en <<- NA})
-  if (is.null(organisation_en)) { organisation_en <<- NA}
+  organisation_en <- NA
+  try(organisation_en <- course_details[["cpCourseDto"]][["organisationResponsibleDto"]][["name"]][["translations"]][["translation"]][[2]][["value"]],silent = TRUE)
+  if (is.null(organisation_en)) { organisation_en <- NA}
   
-  tryCatch(type <- course_details[["cpCourseDto"]][["courseTypeDto"]][["key"]],error=function(err){type <<- NA} ,warning=function(err){type <<- NA})
-  if (is.null(type)) { type <<- NA}
+  type <- NA
+  try(type <- course_details[["cpCourseDto"]][["courseTypeDto"]][["key"]],silent = TRUE)
+  if (is.null(type)) { type <- NA}
   
-  tryCatch(language <- course_details[["cpCourseDto"]][["courseLanguageDtos"]][[1]][["languageDto"]][["key"]],error=function(err){language <<- NA} ,warning=function(err){language <<- NA})
-  if (is.null(language)) { language <<- NA}
+  language <- NA
+  try(language <- course_details[["cpCourseDto"]][["courseLanguageDtos"]][[1]][["languageDto"]][["key"]],silent = TRUE)
+  if (is.null(language)) { language <- NA}
   
-  tryCatch(semesterhours <- course_details[["cpCourseDto"]][["courseNormConfigs"]][[1]][["value"]],error=function(err){semesterhours <<- NA} ,warning=function(err){semesterhours <<- NA})
-  if (is.null(semesterhours)) { semesterhours <<- NA}
+  semesterhours <- NA
+  try(semesterhours <- course_details[["cpCourseDto"]][["courseNormConfigs"]][[1]][["value"]],silent = TRUE)
+  if (is.null(semesterhours)) { semesterhours <- NA}
   
-  tryCatch(teacher_list <- course_details[["cpCourseDto"]][["lectureships"]],error=function(err){teacher_list <<- NA} ,warning=function(err){teacher_list <<- NA})
+  try(teacher_list <- course_details[["cpCourseDto"]][["lectureships"]],silent = TRUE)
   teachers <- ""
   for (teacher in teacher_list) {
     teacher_first_name <- teacher[["identityLibDto"]][["firstName"]]
@@ -50,47 +61,61 @@ scrape_data <- function(course_id){
     teachers <- paste(teachers,teacher_full_name,", ", sep="")
   } 
   
-  tryCatch(content_ge <- course_details[["cpCourseDescriptionDto"]][["courseContent"]][["translations"]][["translation"]][[1]][["value"]],error=function(err){content_ge <<- NA} ,warning=function(err){content_ge <<- NA})
-  if (is.null(content_ge)) { content_ge <<- NA}
+  content_ge <- NA
+  try(content_ge <- course_details[["cpCourseDescriptionDto"]][["courseContent"]][["translations"]][["translation"]][[1]][["value"]],silent = TRUE)
+  if (is.null(content_ge)) { content_ge <- NA}
   
-  tryCatch(content_en <- course_details[["cpCourseDescriptionDto"]][["courseContent"]][["translations"]][["translation"]][[2]][["value"]],error=function(err){content_en <<- NA} ,warning=function(err){content_en <<- NA})
-  if (is.null(content_en)) { content_en <<- NA}
+  content_en <- NA
+  try(content_en <- course_details[["cpCourseDescriptionDto"]][["courseContent"]][["translations"]][["translation"]][[2]][["value"]],silent = TRUE)
+  if (is.null(content_en)) { content_en <- NA}
   
-  tryCatch(previous_knowledge_ge <- course_details[["cpCourseDescriptionDto"]][["previousKnowledge"]][["translations"]][["translation"]][[1]][["value"]],error=function(err){previous_knowledge_ge <<- NA} ,warning=function(err){previous_knowledge_ge <<- NA})
-  if (is.null(previous_knowledge_ge)) { previous_knowledge_ge <<- NA}
+  previous_knowledge_ge <- NA
+  try(previous_knowledge_ge <- course_details[["cpCourseDescriptionDto"]][["previousKnowledge"]][["translations"]][["translation"]][[1]][["value"]],silent = TRUE)
+  if (is.null(previous_knowledge_ge)) { previous_knowledge_ge <- NA}
   
-  tryCatch(previous_knowledge_en <- course_details[["cpCourseDescriptionDto"]][["previousKnowledge"]][["translations"]][["translation"]][[2]][["value"]],error=function(err){previous_knowledge_en <<- NA} ,warning=function(err){previous_knowledge_en <<- NA})
-  if (is.null(previous_knowledge_en)) { previous_knowledge_en <<- NA}
+  previous_knowledge_en <- NA
+  try(previous_knowledge_en <- course_details[["cpCourseDescriptionDto"]][["previousKnowledge"]][["translations"]][["translation"]][[2]][["value"]],silent = TRUE)
+  if (is.null(previous_knowledge_en)) { previous_knowledge_en <- NA}
   
-  tryCatch(objective_ge <- course_details[["cpCourseDescriptionDto"]][["courseObjective"]][["translations"]][["translation"]][[1]][["value"]],error=function(err){objective_ge <<- NA} ,warning=function(err){objective_ge <<- NA})
-  if (is.null(objective_ge)) { objective_ge <<- NA}
+  objective_ge <- NA
+  try(objective_ge <- course_details[["cpCourseDescriptionDto"]][["courseObjective"]][["translations"]][["translation"]][[1]][["value"]],silent = TRUE)
+  if (is.null(objective_ge)) { objective_ge <- NA}
   
-  tryCatch(objective_en <- course_details[["cpCourseDescriptionDto"]][["courseObjective"]][["translations"]][["translation"]][[2]][["value"]],error=function(err){objective_en <<- NA} ,warning=function(err){objective_en <<- NA})
-  if (is.null(objective_en)) { objective_en <<- NA}
+  objective_en <- NA
+  try(objective_en <- course_details[["cpCourseDescriptionDto"]][["courseObjective"]][["translations"]][["translation"]][[2]][["value"]],silent = TRUE)
+  if (is.null(objective_en)) { objective_en <- NA}
   
-  tryCatch(teaching_method_ge <- course_details[["cpTeachingMethodDto"]][["name"]][["translations"]][["translation"]][[1]][["value"]],error=function(err){teaching_method_ge <<- NA} ,warning=function(err){teaching_method_ge <<- NA})
-  if (is.null(teaching_method_ge)) { teaching_method_ge <<- NA}
+  teaching_method_ge <- NA
+  try(teaching_method_ge <- course_details[["cpTeachingMethodDto"]][["name"]][["translations"]][["translation"]][[1]][["value"]],silent = TRUE)
+  if (is.null(teaching_method_ge)) { teaching_method_ge <- NA}
   
-  tryCatch(teaching_method_en <- course_details[["cpTeachingMethodDto"]][["name"]][["translations"]][["translation"]][[2]][["value"]],error=function(err){teaching_method_en <<- NA} ,warning=function(err){teaching_method_en <<- NA})
-  if (is.null(teaching_method_en)) { teaching_method_en <<- NA}
+  teaching_method_en <- NA
+  try(teaching_method_en <- course_details[["cpTeachingMethodDto"]][["name"]][["translations"]][["translation"]][[2]][["value"]],silent = TRUE)
+  if (is.null(teaching_method_en)) { teaching_method_en <- NA}
   
-  tryCatch(assessment_method_ge <- course_details[["cpCourseDescriptionDto"]][["assessmentScheme"]][["translations"]][["translation"]][[1]][["value"]],error=function(err){assessment_method_ge <<- NA} ,warning=function(err){assessment_method_ge <<- NA})
-  if (is.null(assessment_method_ge)) { assessment_method_ge <<- NA}
+  assessment_method_ge <- NA
+  try(assessment_method_ge <- course_details[["cpCourseDescriptionDto"]][["assessmentScheme"]][["translations"]][["translation"]][[1]][["value"]],silent = TRUE)
+  if (is.null(assessment_method_ge)) { assessment_method_ge <- NA}
   
-  tryCatch(assessment_method_en <- course_details[["cpCourseDescriptionDto"]][["assessmentScheme"]][["translations"]][["translation"]][[2]][["value"]],error=function(err){assessment_method_en <<- NA} ,warning=function(err){assessment_method_en <<- NA})
-  if (is.null(assessment_method_en)) { assessment_method_en <<- NA}
+  assessment_method_en <- NA
+  try(assessment_method_en <- course_details[["cpCourseDescriptionDto"]][["assessmentScheme"]][["translations"]][["translation"]][[2]][["value"]],silent = TRUE)
+  if (is.null(assessment_method_en)) { assessment_method_en <- NA}
   
-  tryCatch(recommended_literature_ge <- course_details[["cpCourseDescriptionDto"]][["additionalInformation"]][["recommendedLiterature"]][["translations"]][["translation"]][[1]][["value"]],error=function(err){recommended_literature_ge <<- NA} ,warning=function(err){recommended_literature_ge <<- NA})
-  if (is.null(recommended_literature_ge)) { recommended_literature_ge <<- NA}
+  recommended_literature_ge <- NA
+  try(recommended_literature_ge <- course_details[["cpCourseDescriptionDto"]][["additionalInformation"]][["recommendedLiterature"]][["translations"]][["translation"]][[1]][["value"]],silent = TRUE)
+  if (is.null(recommended_literature_ge)) { recommended_literature_ge <- NA}
   
-  tryCatch(recommended_literature_en <- course_details[["cpCourseDescriptionDto"]][["additionalInformation"]][["recommendedLiterature"]][["translations"]][["translation"]][[2]][["value"]],error=function(err){recommended_literature_en <<- NA} ,warning=function(err){recommended_literature_en <<- NA})
-  if (is.null(recommended_literature_en)) { recommended_literature_en <<- NA}
+  recommended_literature_en <- NA
+  try(recommended_literature_en <- course_details[["cpCourseDescriptionDto"]][["additionalInformation"]][["recommendedLiterature"]][["translations"]][["translation"]][[2]][["value"]],silent = TRUE)
+  if (is.null(recommended_literature_en)) { recommended_literature_en <- NA}
   
-  tryCatch(additional_comments_ge <- course_details[["cpCourseDescriptionDto"]][["additionalInformation"]][["comments"]][["translations"]][["translation"]][[1]][["value"]],error=function(err){additional_comments_ge <<- NA} ,warning=function(err){additional_comments_ge <<- NA})
-  if (is.null(additional_comments_ge)) { additional_comments_ge <<- NA}
+  additional_comments_ge <- NA
+  try(additional_comments_ge <- course_details[["cpCourseDescriptionDto"]][["additionalInformation"]][["comments"]][["translations"]][["translation"]][[1]][["value"]],silent = TRUE)
+  if (is.null(additional_comments_ge)) { additional_comments_ge <- NA}
   
-  tryCatch(additional_comments_en <- course_details[["cpCourseDescriptionDto"]][["additionalInformation"]][["comments"]][["translations"]][["translation"]][[2]][["value"]],error=function(err){additional_comments_en <<- NA} ,warning=function(err){additional_comments_en <<- NA})
-  if (is.null(additional_comments_en)) { additional_comments_en <<- NA}
+  additional_comments_en <- NA
+  try(additional_comments_en <- course_details[["cpCourseDescriptionDto"]][["additionalInformation"]][["comments"]][["translations"]][["translation"]][[2]][["value"]],silent = TRUE)
+  if (is.null(additional_comments_en)) { additional_comments_en <- NA}
   
   new_course <- tibble(id, course_number, semester, title_ge, title_en, ECTS, organisation_ge, organisation_en, type, language, semesterhours, teachers, content_ge, content_en, previous_knowledge_ge,  previous_knowledge_en, objective_ge, objective_en, teaching_method_ge, teaching_method_en, assessment_method_ge,  assessment_method_en, recommended_literature_ge, recommended_literature_en,additional_comments_ge, additional_comments_en)
   
